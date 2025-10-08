ae0eea00725d6322f885616c4393655bd6137aa8
chandinidevi06 <eegala.chandini2004@gmail.com>
2025-10-08 21:10:36 +0530

    added the file

--- Routes/user-Routes.js
+++ Routes/user-Routes.js
@@ -1,11 +1,23 @@
-var express=require("express")
+// var express=require("express")
 
-var{register,login}=require("../Controllers/user-Controllers")
-var router=express.Router()
+// var{register,login}=require("../Controllers/user-Controllers")
+// var router=express.Router()
 
 
-router.post("/register",register)
+// router.post("/register",register)
 
-router.get("/login",login)
+// router.get("/login",login)
 
-module.exports=router
+// module.exports=router
+
+
+
+
+const express = require("express");
+const router = express.Router();
+const { register, login } = require("../Controllers/user-Controllers");
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-require("dotenv").config()
-var express=require("express")
+// // require("dotenv").config()
+// // var express=require("express")
 
-var connectToDatabase=require("./Database/db")
-var userRoute=require("./Routes/user-Routes")
+// // var connectToDatabase=require("./Database/db")
+// // var userRoute=require("./Routes/user-Routes")
 
-var app=express()
-
-
-
-connectToDatabase()
+// // var app=express()
 
 
 
-app.use(express.json())
+// // connectToDatabase()
 
-app.use("api/auth",userRoute)
+// // app.use(express.json())
 
-var PORT=process.env.PORT || 3000
+// // app.use("api/auth",userRoute)
 
-app.listen(PORT,()=>{
-    console.log("the server is running");
-})
+// // var PORT=process.env.PORT || 3000
+
+// // app.listen(PORT,()=>{
+// //     console.log("the server is running");
+// // })
 
 
 
@@ -32,3 +30,41 @@ app.listen(PORT,()=>{
 
 
 
+// require("dotenv").config();
+// const express = require("express");
+
+// const connectToDatabase = require("./Database/db");
+// const userRoute = require("./Routes/user-Routes");
+
+// const app = express();
+
+// connectToDatabase();
+
+// app.use(express.json());
+
+
+// app.use("/api/auth", userRoute);
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`The server is running on port ${PORT}`);
+// });
+
+
+
+require("dotenv").config();
+const express = require("express");
+const connectToDatabase = require("./Database/db");
+const userRoutes = require("./Routes/user-Routes");
+
+const app = express();
+connectToDatabase();
+
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
